b5f4557b26ad7245ebe99e4abe7c2bb9d251ec55
hemanth <applicationcomputer257@gmail.com>
2024-09-10 21:10:23 +0530

    responsiveness changes on small screen feature slider v3

--- components/SmallScreenFeatureSlider.tsx
+++ components/SmallScreenFeatureSlider.tsx
@@ -34,11 +34,11 @@ export const SmallScreenFeatureSlider = () => {
     >
       <div className="flex flex-col justify-center items-center space-y-1.5  sm:space-y-2">
         <span className="text-[#BABBBA] text-sm">features</span>
-        <h2 className="text-[3rem] sm:text-[3.7rem] font-semibold   text-center px-2.5 ">
+        <h2 className="text-[3rem] sm:text-[3.7rem] font-semibold  text-center px-2.5 ">
           Your Portfolio Your Way
         </h2>
         <p
-          className={`text-sm md:text-lg tracking-wide text-black py-2 ${newAgeFontBold.className}`}
+          className={`text-sm md:text-lg tracking-wide text-black py-2 text-center ${newAgeFontBold.className}`}
         >
           Create a stunning portfolio in minutes, not hours.
         </p>
@@ -54,11 +54,7 @@ export const SmallScreenFeatureSlider = () => {
       </div>
       <div
         className="flex mt-8 w-full flex-col justify-center items-center 
-space-y-8
-        sm:space-y-12
-        md:space-y-16
-
-     "
+space-y-8 sm:space-y-12 md:space-y-16 "
       >
         {slideContents.map((content, index) => (
           <SlideContent {...content} key={index} />
@@ -85,7 +81,7 @@ const SlideContent = ({
       <div className="flex flex-col w-full ">
         <div className="w-full h-52 sm:h-72 md:h-72 rounded-2xl bg-[#EFEFEE] border" />
         <div className="flex pl-6  pt-4 flex-col text-black">
-          <p className="text-base  sm:text-xl md:text-2xl font-light capitalize">
+          <p className="text-base  sm:text-xl md:text-2xl font-light capitalize text-center">
             {description}
           </p>
         </div>
